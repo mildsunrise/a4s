d98512682c81e144d8fa7397cb76dd62425a3725
Alba Mendez <me@alba.sh>
2019-09-05 19:26:30 +0200

    Write more tests

---
+++ test/core.test.ts
@@ -0,0 +1,60 @@
+import { formatTimestamp, formatDate, getSigningData, signDigest } from "../src/core";
+
+describe('Core signing', () => {
+
+    it('generates timestamps correctly', () => {
+        expect(formatTimestamp(new Date(1935346573456))).toBe('20310430T201613Z')
+        expect(formatTimestamp()).toMatch(/^20\d{6}T\d{6}Z$/)
+    })
+
+    it('generates datestamps correctly', () => {
+        expect(formatDate(new Date(1935346573456))).toBe('20310430')
+        expect(formatDate()).toMatch(/^20\d{6}$/)
+    })
+
+    it('derives a key correctly', () => {
+        expect(getSigningData('20310430', 'test', 'eu-west-3', 'service')).toStrictEqual(
+            {
+                scope: '20310430/eu-west-3/service/aws4_request',
+                key: Buffer.from('674f7e8745f37c14fe188810745a0c54d78824d747e69eb74396c369ae5f7b4d', 'hex')
+            }
+        )
+    })
+
+    it('simple cache works correctly', () => {
+        const derive: any = getSigningData.makeSimpleCache()
+        const key1 = {
+            args: ['20310430', 'test', 'eu-west-3', 'service'],
+            result: {
+                scope: '20310430/eu-west-3/service/aws4_request',
+                key: Buffer.from('674f7e8745f37c14fe188810745a0c54d78824d747e69eb74396c369ae5f7b4d', 'hex')
+            }
+        }
+        const key2 = {
+            args: ['20310431', 'test', 'eu-west-3', 'service'],
+            result: {
+                scope: '20310431/eu-west-3/service/aws4_request',
+                key: Buffer.from('2bcc47ebfe90545ac1b8c8fe9a76a8a4a066cbae64b8b57a27a74def2cf5bafe', 'hex')
+            }
+        }
+        expect(derive(...key1.args)).toStrictEqual(key1.result)
+        expect(derive(...key1.args)).toStrictEqual(key1.result)
+        expect(derive(...key2.args)).toStrictEqual(key2.result)
+        expect(derive(...key1.args)).toStrictEqual(key1.result)
+        expect(derive(...key2.args)).toStrictEqual(key2.result)
+        expect(derive(...key2.args)).toStrictEqual(key2.result)
+    })
+
+    it('signs a payload digest correctly', () => {
+        expect(signDigest(
+            'ALG',
+            '2e1cf7ed91881a30569e46552437e4156c823447bf1781b921b5d486c568dd1c',
+            '20310430T201613Z',
+            {
+                scope: '20310430/eu-west-3/service/aws4_request',
+                key: Buffer.from('674f7e8745f37c14fe188810745a0c54d78824d747e69eb74396c369ae5f7b4d', 'hex')
+            },
+        ).toString('hex')).toBe('f32bf1ce9850de71499f36962e445aba4ef65870af1688974b4f4426f685aac3')
+    })
+
+})
--- test/http.test.ts
+++ test/http.test.ts
@@ -1,65 +1,99 @@
-import { signRequest, autoSignRequest } from '../src/http'
+import { signRequest, autoSignRequest, getCanonicalURI } from '../src/http'
 
 describe('HTTP signing', () => {
-    const credentials = { accessKey: 'access_test', secretKey: 'secret_test' }
 
-    it('signs a GET request with only header x-amz-date', () => {
-        const newHeaders = signRequest(
-            { ...credentials, serviceName: 'ec2', regionName: 'us-east-1' },
-            'GET', 'https://ec2.amazonaws.com?Action=DescribeRegions&Version=2013-10-15',
-            {
-                'x-amz-date': '20190830T164820Z',
-            }
-        )
-        expect(newHeaders.authorization).toBe('AWS4-HMAC-SHA256 Credential=access_test/20190830/us-east-1/ec2/aws4_request, SignedHeaders=host;x-amz-date, Signature=afc0e2557e1c0bc4d8cb15aaf8df57d862fda39caa9cc574215f10987816710c')
+    describe('canonical URI', () => {
+        it('returns / for empty string', () =>
+            expect(getCanonicalURI('')).toBe('/'))
+        it('appends slash if needed', () =>
+            expect(getCanonicalURI('a')).toBe('/a'))
+        it('appends slash to percent-encoded slash', () =>
+            expect(getCanonicalURI('%2f')).toBe('/%252F'))
+        it('condenses multiple slashes, «.», «..» and percent-encoded', () =>
+            expect(getCanonicalURI('b/////.//..///%2E///%2E///%2E.///.%2E///c')).toBe('/c'))
+        it('doesn\'t condense percent-encoded slash', () =>
+            expect(getCanonicalURI('//%2f//')).toBe('/%252F/'))
+        it('resolves . and .. correctly', () =>
+            expect(getCanonicalURI('/a/b/../c/%2E./d')).toBe('/a/d'))
+        it('preserves trailing slash', () => {
+            expect(getCanonicalURI('../../../c')).toBe('/c')
+            expect(getCanonicalURI('../../../c/')).toBe('/c/')
+            expect(getCanonicalURI('../../../c/.')).toBe('/c/')
+            expect(getCanonicalURI('a/b/..')).toBe('/a/')
+            expect(getCanonicalURI('a/%2f')).toBe('/a/%252F')
+            expect(getCanonicalURI('a/%2f/.')).toBe('/a/%252F/')
+        })
+        it('returns uppercase percent-encodes', () =>
+            expect(getCanonicalURI('/test%0a')).toBe('/test%250A'))
+        it('un-encodes unreserved characters', () =>
+            expect(getCanonicalURI('/test%41')).toBe('/testA'))
+        it('encodes characters if needed', () =>
+            expect(getCanonicalURI('/test\n')).toBe('/test%250A'))
+        it('encodes in UTF-8', () =>
+            expect(getCanonicalURI('/test😊')).toBe('/test%25F0%259F%2598%258A'))
     })
 
-    it('auto-signs the request inferring service/region', () => {
-        const request = autoSignRequest(
-            credentials,
-            {
-                host: 'ec2.amazonaws.com',
-                path: '?Action=DescribeRegions&Version=2013-10-15',
-                headers: {
-                    'x-amz-date': '20190830T164820Z',
-                },
-            },
-            ''
-        )
-        expect(request.headers!.authorization).toBe('AWS4-HMAC-SHA256 Credential=access_test/20190830/us-east-1/ec2/aws4_request, SignedHeaders=host;x-amz-date, Signature=afc0e2557e1c0bc4d8cb15aaf8df57d862fda39caa9cc574215f10987816710c')
-    })
+    describe('signing / autosigning', () => {
+        const credentials = { accessKey: 'access_test', secretKey: 'secret_test' }
 
-    it('auto-signs the request inferring the endpoint (without region)', () => {
-        const request = autoSignRequest(
-            { ...credentials, serviceName: 'ec2' },
-            {
-                path: '?Action=DescribeRegions&Version=2013-10-15',
-                headers: {
+        it('signs a GET request with only header x-amz-date', () => {
+            const newHeaders = signRequest(
+                { ...credentials, serviceName: 'ec2', regionName: 'us-east-1' },
+                'GET', 'https://ec2.amazonaws.com?Action=DescribeRegions&Version=2013-10-15',
+                {
                     'x-amz-date': '20190830T164820Z',
-                },
-            },
-            Buffer.from('')
-        )
-        expect(request.headers!.host).toBe('ec2.amazonaws.com')
-        expect(request.hostname).toBe('ec2.amazonaws.com')
-        expect(request.headers!.authorization).toBe('AWS4-HMAC-SHA256 Credential=access_test/20190830/us-east-1/ec2/aws4_request, SignedHeaders=host;x-amz-date, Signature=afc0e2557e1c0bc4d8cb15aaf8df57d862fda39caa9cc574215f10987816710c')
-    })
+                }
+            )
+            expect(newHeaders.authorization).toBe('AWS4-HMAC-SHA256 Credential=access_test/20190830/us-east-1/ec2/aws4_request, SignedHeaders=host;x-amz-date, Signature=afc0e2557e1c0bc4d8cb15aaf8df57d862fda39caa9cc574215f10987816710c')
+        })
 
-    it('auto-signs a POST with body', () => {
-        const request = autoSignRequest(
-            credentials,
-            {
-                method: 'POST',
-                host: 'dynamodb.us-west-2.amazonaws.com',
-                headers: {
-                    'x-amz-date': '20190830T164820Z',
-                    'content-type': 'application/x-amz-json-1.0',
-                    'x-amz-target': 'DynamoDB_20120810.CreateTable',
+        it('auto-signs the request inferring service/region', () => {
+            const request = autoSignRequest(
+                credentials,
+                {
+                    host: 'ec2.amazonaws.com',
+                    path: '?Action=DescribeRegions&Version=2013-10-15',
+                    headers: {
+                        'x-amz-date': '20190830T164820Z',
+                    },
                 },
-            },
-            '{"KeySchema": [{"KeyType": "HASH","AttributeName": "Id"}],"TableName": "TestTable","AttributeDefinitions": [{"AttributeName": "Id","AttributeType": "S"}],"ProvisionedThroughput": {"WriteCapacityUnits": 5,"ReadCapacityUnits": 5}}',
-        )
-        expect(request.headers!.authorization).toBe('AWS4-HMAC-SHA256 Credential=access_test/20190830/us-west-2/dynamodb/aws4_request, SignedHeaders=content-type;host;x-amz-date;x-amz-target, Signature=c34b85b7c550eaa329ac72987fb54d77889c5f85e9bf1df213e1ae8db2d5402c')
+                ''
+            )
+            expect(request.headers!.authorization).toBe('AWS4-HMAC-SHA256 Credential=access_test/20190830/us-east-1/ec2/aws4_request, SignedHeaders=host;x-amz-date, Signature=afc0e2557e1c0bc4d8cb15aaf8df57d862fda39caa9cc574215f10987816710c')
+        })
+
+        it('auto-signs the request inferring the endpoint (without region)', () => {
+            const request = autoSignRequest(
+                { ...credentials, serviceName: 'ec2' },
+                {
+                    path: '?Action=DescribeRegions&Version=2013-10-15',
+                    headers: {
+                        'x-amz-date': '20190830T164820Z',
+                    },
+                },
+                Buffer.from('')
+            )
+            expect(request.headers!.host).toBe('ec2.amazonaws.com')
+            expect(request.hostname).toBe('ec2.amazonaws.com')
+            expect(request.headers!.authorization).toBe('AWS4-HMAC-SHA256 Credential=access_test/20190830/us-east-1/ec2/aws4_request, SignedHeaders=host;x-amz-date, Signature=afc0e2557e1c0bc4d8cb15aaf8df57d862fda39caa9cc574215f10987816710c')
+        })
+
+        it('auto-signs a POST with body', () => {
+            const request = autoSignRequest(
+                credentials,
+                {
+                    method: 'POST',
+                    host: 'dynamodb.us-west-2.amazonaws.com',
+                    headers: {
+                        'x-amz-date': '20190830T164820Z',
+                        'content-type': 'application/x-amz-json-1.0',
+                        'x-amz-target': 'DynamoDB_20120810.CreateTable',
+                    },
+                },
+                '{"KeySchema": [{"KeyType": "HASH","AttributeName": "Id"}],"TableName": "TestTable","AttributeDefinitions": [{"AttributeName": "Id","AttributeType": "S"}],"ProvisionedThroughput": {"WriteCapacityUnits": 5,"ReadCapacityUnits": 5}}',
+            )
+            expect(request.headers!.authorization).toBe('AWS4-HMAC-SHA256 Credential=access_test/20190830/us-west-2/dynamodb/aws4_request, SignedHeaders=content-type;host;x-amz-date;x-amz-target, Signature=c34b85b7c550eaa329ac72987fb54d77889c5f85e9bf1df213e1ae8db2d5402c')
+        })
     })
 
 })
