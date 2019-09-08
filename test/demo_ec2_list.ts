62bc1c3e1d9bc62f89102d23909b66c65f66ea87
Alba Mendez <me@alba.sh>
2019-09-08 23:38:23 +0200

    💡 Add EC2 demo

---
+++ test/demo_ec2_list.ts
@@ -0,0 +1,34 @@
+/**
+ * Demo CLI tool that makes a LIST request to a bucket.
+ */
+
+import { get } from 'https'
+import { URLSearchParams } from 'url'
+import { signRequest } from '../src/http'
+import { toRequestOptions } from '../src/util/request'
+
+const accessKey = process.env.AWS_ACCESS_KEY_ID!
+const secretKey = process.env.AWS_SECRET_ACCESS_KEY!
+const args = process.argv.slice(2)
+if (!accessKey || !secretKey || args.length !== 1) {
+    console.error(`Usage: demo_ec2_list.js <region>`)
+    console.error('Please make sure AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY are set')
+    process.exit(1)
+}
+
+const credentials = { accessKey, secretKey, serviceName: 'ec2', regionName: args[0] }
+const request = {
+    url: {
+        searchParams: new URLSearchParams({
+            Action: 'DescribeInstances',
+            Version: '2016-11-15',
+        }),
+    },
+}
+signRequest(credentials, request, { set: true })
+
+console.log('Sending request:', request)
+get(toRequestOptions(request), response => {
+    console.log(`Got ${response.statusCode} response:`)
+    response.pipe(process.stdout)
+})
