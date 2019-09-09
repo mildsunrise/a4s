d4f82b54c0b7f0b3ec979e861fbf5dc73a1a435a
Alba Mendez <me@alba.sh>
2019-09-09 16:47:15 +0200

    Fix index

--- src/index.ts
+++ src/index.ts
@@ -3,5 +3,6 @@ import * as http from './http'
 import * as s3 from './s3'
 import * as util from './util'
 
-export { signRequest, autoSignRequest } from './http'
+export { toURL, toRequestOptions } from './util/request' 
+export { signRequest } from './http'
 export { core, http, s3, util }
